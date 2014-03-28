276181953e2e76ff9e24ca22ec5ce91f955a787b
Deniss Strogonovs <denis.strogonov@gmail.com>
2014-03-28 20:01:48 +0200

    Change

--- script.sh
+++ script.sh
@@ -2,8 +2,7 @@
 git filter-branch --env-filter \
     'if [ $GIT_COMMIT = 3c6ccddbc62754ae5eae9631b0eb4a68ba6398a2 ]
      then
-         export GIT_AUTHOR_DATE="Fri Jan 2 21:38:53 2009 -0800"
-         export GIT_COMMITTER_DATE="Sat May 19 01:01:01 2007 -0700"
+         export GIT_AUTHOR_DATE="Fri Jan 3 21:38:53 2014 +0200"
      fi'
 	 
-	 git filter-branch --env-filter \ 'if [ $GIT_COMMIT = ea486a9 ] then export GIT_COMMIT_DATE = "Sat May 19 01:01:01 2007 -0700" fi'
+	
